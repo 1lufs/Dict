2e6ad2d68d0fadd57bc8e199dfdd8b620b144f3f
1lufs <akim010199@gmail.com>
2025-05-15 01:27:53 +0300

    bv

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import search_view
 
 urlpatterns = [
+    path('search/', search_view, name='search'),
     path('', views.post_list, name='post_list'),
     path('word/<int:pk>/', views.post_list, name='word_detail'),
     path('word/new/', views.word_new, name='word_new'),
     path('word/<int:pk>/edit/', views.word_edit, name='word_edit')
 ]
+
